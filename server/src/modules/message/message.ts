4ee99a8e60835e52cd5bd29c541e7356e6e27de0
Spoookynator <99873987+Spoookynator@users.noreply.github.com>
2025-06-04 19:50:34 +0200

    tiny writing mistake

--- server/src/modules/message/message.ts
+++ server/src/modules/message/message.ts
@@ -1,174 +1,190 @@
 import { UUID } from "crypto";
 import { Database } from "../database/database";
-import { UserManager } from "../userManager/userManager";
+import UserManager from "../userManager/userManager";
 import { Action, Message, ResponseContent } from "@anocm/shared/dist";
-import { validate } from "uuid";
+import { validate } from "uuid"
 
-export async function broadcastToChat(message: Message) {
-  const res = await Database.getChat(message.chatID);
+export async function broadcastToChat(message: Message, database: Database, handler: UserManager) {
+    const res = await database.getChat(message.chatID);
 
-  if (res === false) {
-    UserManager.sendMessage(
-      message.senderID,
-      messageResponse(message.senderID, {
-        success: false,
-        message: `Database Error`,
-      })
-    );
-    return;
-  }
 
-  if (!(message.senderID in res.chatUserList)) {
-    UserManager.sendMessage(
-      message.senderID,
-      messageResponse(message.senderID, {
-        success: false,
-        message: `Message could not be broadcasted to chat with id '${message.chatID}'`,
-      })
-    );
-    return;
-  }
+    if (res === false) {
+        handler.sendMessage(message.senderID, messageResponse(
+            message.senderID,
+            {
+                success: false,
+                message: `Database Error`
+            }
 
-  let successful: number = 0;
-
-  Object.keys(res.chatUserList).forEach((id) => {
-    if (isUUID(id)) {
-      const uuid: UUID = id;
-      UserManager.sendMessage(uuid, message);
-      successful++;
+        ))
+        return;
     }
-  });
 
-  UserManager.sendMessage(
-    message.senderID,
-    messageResponse(message.senderID, {
-      success: true,
-      message: `Message broadcasted to ${successful} Clients in '${message.chatID}'`,
-    })
-  );
+    if (!(message.senderID in res.chatUserList)) {
+        handler.sendMessage(message.senderID, messageResponse(
+            message.senderID,
+            {
+                success: false,
+                message: `Message could not be broadcasted to chat with id '${message.chatID}'`
+            }
+
+        ))
+        return;
+    }
+
+    let sucessful: number = 0;
+
+    Object.keys(res.chatUserList).forEach(id => {
+        if (isUUID(id)) {
+            const uuid: UUID = id;
+            handler.sendMessage(uuid, message);
+            sucessful++;
+        }
+    });
+
+    handler.sendMessage(
+        message.senderID,
+        messageResponse(message.senderID,
+            {
+                success: true,
+                message: `Message broadcasted to ${sucessful} Clients in '${message.chatID}'`,
+            }
+
+        )
+    );
 }
+
 
 //#region Unsafe
 
 /**
- *
+ * 
  * @param message Message object (here content has to be a valid UUID)
  * @param database Database object
- * @returns
+ * @param handler UserManager
+ * @returns 
  */
-export async function addToChatNoConfirm(message: Message) {
-  let receiver = isUUID(message.content) ? message.content : null;
+export async function addToChatNoConfirm(message: Message, database: Database, handler: UserManager) {
 
-  if (receiver === null) {
-    UserManager.sendMessage(
-      message.senderID,
-      messageResponse(message.senderID, {
-        success: false,
-        message: "Wrong format for content, should be UUID",
-      })
-    );
+    let reciever = (isUUID(message.content)) ? message.content : null;
 
-    return;
-  }
+    if (reciever === null) {
+        handler.sendMessage(message.senderID, messageResponse(
+            message.senderID,
+            {
+                sucess: false,
+                message: "Wrong format for content, should be UUID"
+            },
 
-  const res = await Database.addUsertoChat(message.chatID, receiver);
+        ));
 
-  if (res === false) {
-    UserManager.sendMessage(
-      message.senderID,
-      messageResponse(message.senderID, {
-        success: false,
-        message: "Could not add user to chat. User or chat may be invalid",
-      })
-    );
+        return;
+    }
 
-    return;
-  } else {
-    UserManager.sendMessage(
-      message.senderID,
-      messageResponse(message.senderID, {
-        success: true,
-        message: `Added client ${message.content} to chat ${message.chatID}`,
-      })
-    );
-  }
+    const res = await database.addUsertoChat(message.chatID, reciever);
+
+    if (res === false) {
+        handler.sendMessage(message.senderID, messageResponse(
+            message.senderID,
+            {
+                sucess: false,
+                message: "Could not add user to chat. User or chat may be invalid"
+            }
+        ));
+
+        return;
+    } else {
+        handler.sendMessage(message.senderID, messageResponse(
+            message.senderID,
+            {
+                sucess: true,
+                message: `Added client ${message.content} to chat ${message.chatID}`
+            }
+        ))
+    }
 }
+
 
 // basically the same as add, might be able to save some code work but am too lazy rn
 /**
- *
+ * 
  * @param message Message object (here content has to be a valid UUID)
  * @param database Database object
- * @returns
+ * @param handler UserManager
+ * @returns 
  */
-export async function removeFromChatNoConfirm(message: Message) {
-  let reciever = isUUID(message.content) ? message.content : null;
+export async function removeFromChatNoConfirm(message: Message, database: Database, handler: UserManager) {
+    let reciever = (isUUID(message.content)) ? message.content : null;
 
-  if (reciever === null) {
-    UserManager.sendMessage(
-      message.senderID,
-      messageResponse(message.senderID, {
-        success: false,
-        message: "Wrong format for content, should be UUID",
-      })
-    );
+    if (reciever === null) {
+        handler.sendMessage(message.senderID, messageResponse(
+            message.senderID,
+            {
+                sucess: false,
+                message: "Wrong format for content, should be UUID"
+            },
 
-    return;
-  }
+        ));
 
-  console.log(reciever);
+        return;
+    }
 
-  const res = await Database.deleteUserFromChat(message.chatID, reciever);
+    console.log(reciever);
 
-  console.log(res);
 
-  if (res === false) {
-    UserManager.sendMessage(
-      message.senderID,
-      messageResponse(message.senderID, {
-        success: false,
-        message: "Could not remove user from chat.",
-      })
-    );
+    const res = await database.deleteUserFromChat(message.chatID, reciever);
 
-    return;
-  } else {
-    UserManager.sendMessage(
-      message.senderID,
-      messageResponse(message.senderID, {
-        success: true,
-        message: `Removed user ${message.content} from chat ${message.chatID}`,
-      })
-    );
-  }
+    console.log(res);
+
+    if (res === false) {
+        handler.sendMessage(message.senderID, messageResponse(
+            message.senderID,
+            {
+                sucess: false,
+                message: "Could not remove user from chat."
+            }
+        ));
+
+        return;
+    } else {
+        handler.sendMessage(message.senderID, messageResponse(
+            message.senderID,
+            {
+                sucess: true,
+                message: `Removed user ${message.content} from chat ${message.chatID}`
+            }
+        ))
+    }
 }
 
 //#endregion
+
+
 
 //#region other
 /** 
  this is to assert that if the output is true, value is of type UUID
  */
 function isUUID(value: string): value is UUID {
-  return validate(value);
+    return validate(value);
 }
 
 /**
- *
+ * 
  * @param senderID UUID of the sender
  * @param content of type Response Content, has sucess bool, message and json response
  * @returns Message object
  */
 function messageResponse(senderID: UUID, content: ResponseContent): Message {
-  const msg: Message = {
-    action: Action.MessageResponse,
-    content: JSON.stringify(content),
-    timestamp: Date.now(),
-    senderID: senderID,
-    chatID: senderID,
-  };
+    const msg: Message = {
+        action: Action.MessageResponse,
+        content: JSON.stringify(content),
+        timestamp: Date.now(),
+        senderID: senderID,
+        chatID: senderID
+    };
 
-  return msg;
+    return msg;
 }
 
 //#endregion
