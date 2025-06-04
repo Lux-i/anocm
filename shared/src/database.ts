e9504856a7bcae0c889588b1cc5c6f8864469d6b
Spoookynator <99873987+Spoookynator@users.noreply.github.com>
2025-06-04 21:04:04 +0200

    removed namespace from shared folder to just make it better :)

--- shared/src/database.ts
+++ shared/src/database.ts
@@ -1,31 +1,30 @@
 import { UUID } from "crypto";
 
-export namespace DatabaseTypes {
-  export interface User {
-    userId: string;
-    username?: string;
-  }
-
-  export interface Chat {
-    chatId: string;
-    chatUserList: Record<UUID, string>;
-    chatSettings: any;
-    chatMessages?: any;
-  }
-
-  export interface DatabaseResponse {
-    success: boolean;
-    error?: string;
-    id?: string;
-    userData?: any;
-  }
-
-  export interface ChatMessage {
-    from: UUID;
-    message: string;
-  }
-
-  export type messageStructure = {
-    [key: EpochTimeStamp]: DatabaseTypes.ChatMessage;
-  };
+export interface User {
+  userId: string;
+  username?: string;
 }
+
+export interface Chat {
+  chatId: string;
+  chatUserList: Record<UUID, string>;
+  chatSettings: any;
+  chatMessages?: any;
+}
+
+export interface DatabaseResponse {
+  success: boolean;
+  error?: string;
+  id?: string;
+  userData?: any;
+}
+
+export interface ChatMessage {
+  from: UUID;
+  message: string;
+}
+
+export type messageStructure = {
+  [key: EpochTimeStamp]: ChatMessage;
+};
+
