10d85f3fe23be8016af84510bdbc506b95abba08
DrGendian <kacperektitow06@gmail.com>
2025-06-05 21:31:15 +0200

    feat(database): Added api function for deleting user from chat
    Refs #25

---
+++ server/src/routers/v2.ts
@@ -0,0 +1,11 @@
+// src/routers/v1.ts
+import { Router } from "express";
+import userRouter from "../routes/v2/user";
+import chatRouter from "../routes/v2/chat";
+import { createUserLimiter } from "../index";
+const router = Router();
+
+router.use("/user", createUserLimiter, userRouter());
+router.use("/chat", chatRouter());
+
+export default router;
