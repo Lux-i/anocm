c996d65cbb84d8ff65843e24a7d7006f39cd5f43
Simon Pöschl <poechl.si2004@gmail.com>
2025-06-23 02:47:08 +0200

    removed WebSocketTest and added UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WebSocketTest from './WebSocketTest'
+import AnocmUI from './AnocmUI'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <WebSocketTest />
+      <AnocmUI />
     </>
   )
 }
