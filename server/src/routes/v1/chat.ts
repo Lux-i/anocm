d9647320282331d6c9b33a8e5a86b71858aa1fee
Lux-i <lucjan.lubomski@gmail.com>
2025-06-23 16:22:06 +0200

    fix(type error)

--- server/src/routes/v1/chat.ts
+++ server/src/routes/v1/chat.ts
@@ -8,89 +8,99 @@ const router = express.Router();
 import { UUID } from "crypto";
 
 export default () => {
-    router.post("/newchat", async (req: Request, res: Response) => {
-        console.log("POST Request: new Chat");
-        try {
-            Database.createChat(req.body.userList, req.body.ttl, req.body.minTTL, req.body.maxTTL, req.body.creatorId, req.body.creatorToken).then((chatId: UUID | false) => {
-                if (chatId != false) {
-                    const response: DatabaseResponse = {
-                        success: true,
-                        id: chatId.toString(),
-                    };
-                    res.send(response);
-                } else {
-                    const response: DatabaseResponse = {
-                        success: false,
-                        error: `Error creating Chat`,
-                    };
-                    res.send(response);
-                }
-            });
-        } catch (err: any) {
-            const response: DatabaseResponse = {
-                success: false,
-                error: err,
-            };
-            res.send(response);
-            console.error("Error creating new Chat: ", err);
+  router.post("/newchat", async (req: Request, res: Response) => {
+    console.log("POST Request: new Chat");
+    try {
+      Database.createChat(
+        req.body.userList,
+        req.body.ttl,
+        req.body.minTTL,
+        req.body.maxTTL,
+        req.body.creatorId,
+        req.body.creatorToken
+      ).then((chatId: UUID | false) => {
+        if (chatId != false) {
+          const response: DatabaseResponse = {
+            success: true,
+            id: chatId,
+          };
+          res.send(response);
+        } else {
+          const response: DatabaseResponse = {
+            success: false,
+            error: `Error creating Chat`,
+          };
+          res.send(response);
         }
-    });
+      });
+    } catch (err: any) {
+      const response: DatabaseResponse = {
+        success: false,
+        error: err,
+      };
+      res.send(response);
+      console.error("Error creating new Chat: ", err);
+    }
+  });
 
-    router.get("/getchat", async (req: Request, res: Response) => {
-        try {
-            const chatId = req.query.chatid as string;
+  router.get("/getchat", async (req: Request, res: Response) => {
+    try {
+      const chatId = req.query.chatid as string;
 
-            if (!chatId) {
-                return res.status(400).json({
-                    success: false,
-                    error: "Missing ChatId in query.",
-                });
-            }
+      if (!chatId) {
+        return res.status(400).json({
+          success: false,
+          error: "Missing ChatId in query.",
+        });
+      }
 
-            Database.getChat(chatId!).then((chat: Chat | false) => {
-                const response: DatabaseResponse = {
-                    success: true,
-                    userData: chat,
-                };
-                res.send(response);
-            });
-        } catch (err: any) {
-            const response: DatabaseResponse = {
-                success: false,
-                error: err,
-            };
-            res.send(response);
+      Database.getChat(chatId!, "", "").then((chat: Chat | false) => {
+        const response: DatabaseResponse = {
+          success: true,
+          userData: chat,
+        };
+        res.send(response);
+      });
+    } catch (err: any) {
+      const response: DatabaseResponse = {
+        success: false,
+        error: err,
+      };
+      res.send(response);
+    }
+  });
+
+  router.post("/adduser", async (req: Request, res: Response) => {
+    console.log("POST Request: add User to Chat");
+    try {
+      Database.addUsertoChat(
+        req.body.chatId,
+        req.body.userId,
+        req.body.adminId,
+        req.body.adminToken
+      ).then((response: boolean) => {
+        if (response == true) {
+          const response: DatabaseResponse = {
+            success: true,
+          };
+          res.send(response);
+        } else {
+          const response: DatabaseResponse = {
+            success: false,
+            error: `Error adding User`,
+          };
+          res.send(response);
         }
-    });
+      });
+    } catch (err: any) {
+      const response: DatabaseResponse = {
+        success: false,
+        error: err,
+      };
+      res.send(response);
+      console.error("Error adding User: ", err);
+    }
+  });
 
-    router.post("/adduser", async (req: Request, res: Response) => {
-        console.log("POST Request: add User to Chat");
-        try {
-            Database.addUsertoChat(req.body.chatId, req.body.userId, req.body.adminId, req.body.adminToken).then(
-                (response: boolean) => {
-                    if (response == true) {
-                        const response: DatabaseResponse = {
-                            success: true,
-                        };
-                        res.send(response);
-                    } else {
-                        const response: DatabaseResponse = {
-                            success: false,
-                            error: `Error adding User`,
-                        };
-                        res.send(response);
-                    }
-                }
-            );
-        } catch (err: any) {
-            const response: DatabaseResponse = {
-                success: false,
-                error: err,
-            };
-            res.send(response);
-            console.error("Error adding User: ", err);
-        }
-    });
-
-    return router;
+  return router;
 };
