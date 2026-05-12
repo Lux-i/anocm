97535fabedab02e71d0b4da9cd39f9d9dbeedd85
Lux-i <lucjan.lubomski@gmail.com>
2026-05-12 17:14:44 +0200

    (fix): removed unnecessary usage of API_BASE (as implemented in branch save-frontend)
    this was breaking local hosting and dns-redirects

--- client/src/AnocmUI.tsx
+++ client/src/AnocmUI.tsx
@@ -44,7 +44,7 @@ const API_BASE =
   import.meta.env.VITE_API_BASE_URL || "https://anocm.tomatenbot.com";
 
 //const API_V1 = `${API_BASE}/api/v1`;
-const API_V2 = `${API_BASE}/api/v2`;
+const API_V2 = `/api/v2`;
 const WS_URL = import.meta.env.VITE_WSS_URL || "wss://anocm.tomatenbot.com/ws";
 
 type UIMessage = ChatMessage & {
